e387f0c9df68813dc499717e46123ecc5050552c
pcosin <pcosin@hotmail.com>
2024-04-28 20:23:31 -0300

    creacion de nueva nota sin renderizar la lista

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const $close = document.querySelector('#form-close-button');
 const $notes = document.querySelector('#notes');
 const $placeholder = document.querySelector('#placeholder');
 
+const notes = [];
+
 
 function OpenInput() {
     document.addEventListener('click', function (event) {
@@ -28,6 +30,38 @@ function closeForm() {
         $form.classList.remove('form-open');
         $title.style.display = 'none';
         $buttons.style.display = 'none'
+        resetValues()
 }
+
+
+$submit.addEventListener('click', handlerSubmit);
+
+function handlerSubmit(event) {
+    event.preventDefault(); 
+    const title = $title.value;
+    const text = $text.value;
+    const hasNote = title || text;
+    if(hasNote) {
+        addNote({title, text});
+        resetValues();
+    } 
+}
+
+function resetValues() {
+    $title.value = '';
+    $text.value = '';
+}
+
+function addNote(note) {
+    const newNote = {
+        title: note.title,
+        text: note.text,
+        color: 'white',
+        id: notes.length > 0 ? notes[notes.length - 1].id + 1 : 1
+    }
+    notes.push(newNote);
+}
+
+
 
 OpenInput()
